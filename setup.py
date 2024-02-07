3ca59a9c501e7430f6e6b7104c30323afa52900e
zalkikar <rayzck9@gmail.com>
2024-02-06 20:44:29 -0500

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='mlm-bias',
-    version='0.1.0',
+    version='0.1.1',
     author='Rahul Zalkikar',
     author_email='rayzck9@gmail.com',
     description='Bias Evaluation Methods for Masked Language Models implemented in PyTorch',
